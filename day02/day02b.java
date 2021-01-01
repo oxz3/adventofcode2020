de5f0b8e3e970122042069e3ca6a08356ace66ce
Oliver <oliver@catalystfoundry.com>
2021-01-01 15:09:18 -0800

    Day2 complete.

---
+++ day02/day02b.java
@@ -0,0 +1,80 @@
+package day02;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class day02b {
+    public static void main(String[] args) {
+        final String filename = "/home/me/Projects/adventofcode2020/day02/input.txt";
+        ArrayList<String> inputAL = new ArrayList<>();
+
+        try {
+            File file = new File(filename);
+            Scanner input = new Scanner(file);
+            while (input.hasNextLine()) {
+                inputAL.add(input.nextLine());
+            }
+            input.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found error.");
+            e.printStackTrace();
+        }
+
+        ArrayList<String> ruleAL = new ArrayList<>();
+        ArrayList<String> passwordAL = new ArrayList<>();
+        splitAL (inputAL, ":", ruleAL, passwordAL);
+
+        ArrayList<String> letterAL = new ArrayList<>();
+        ArrayList<String> occurrenceAL = new ArrayList<>();
+        splitAL(ruleAL, " ", occurrenceAL, letterAL);
+
+        ArrayList<Integer> minAL = new ArrayList<>();
+        ArrayList<Integer> maxAL = new ArrayList<>();
+        splitIntAL(occurrenceAL, "-", minAL, maxAL);
+
+        int total = 0;
+        for (int i = 0; i < passwordAL.size(); i++) {
+            String firstPos = String.valueOf(passwordAL.get(i).charAt(minAL.get(i) - 1));
+            String secondPos = String.valueOf(passwordAL.get(i).charAt(maxAL.get(i) - 1));
+
+            if (letterAL.get(i).equals(firstPos) && !(letterAL.get(i).equals(secondPos))) {
+                total++;
+            } else if (!(letterAL.get(i).equals(firstPos)) && letterAL.get(i).equals(secondPos)) {
+                total++;
+            }
+        }
+
+        System.out.println(total);
+    }
+
+    static int countOfChar (String string, String val) {
+        int count = 0;
+        for (int i = 0; i < string.length(); i++) {
+            if (val.equals(String.valueOf(string.charAt(i)))) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    static void splitAL (ArrayList<String> sourceAL, String splitter, ArrayList<String> leftAL, ArrayList<String> rightAL) {
+
+        for (String s : sourceAL) {
+            String[] line = new String[2];
+            line = s.split(splitter);
+            leftAL.add(line[0]);
+            rightAL.add(line[1].trim());
+        }
+    }
+
+    static void splitIntAL (ArrayList<String> sourceAL, String splitter, ArrayList<Integer> leftIntAL, ArrayList<Integer> rightIntAL) {
+        for (String s : sourceAL) {
+            String[] line = new String[2];
+            line = s.split(splitter);
+            leftIntAL.add(Integer.parseInt(line[0]));
+            rightIntAL.add(Integer.parseInt(line[1]));
+        }
+    }
+}
